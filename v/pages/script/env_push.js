68f3a6f9da26c5fd15e2901286a56fcc701051da
amar-jay <64834413+amar-jay@users.noreply.github.com>
2023-07-08 05:36:47 +0000

    urls for nextauth

--- v/pages/script/env_push.js
+++ v/pages/script/env_push.js
@@ -3,25 +3,32 @@ const fs = require("fs")
 const { exec } = require("child_process")
 
 
+const branch = new Promise((resolve, reject) => exec("git branch --show-current", (err, stdout, stderr) => {
+	if (stdout) resolve(stdout.trim())
+	else if (stderr) reject(stderr)
+	else reject(new Error("git branch not found"))
+})
+)
 // vercel add env from .env.local
 
 const pull = async () => {
 	const file = fs.readFileSync(path.resolve(__dirname, "../.env.local"), "utf-8")
-	const lines = file.split("\n").map(line => line.trim()).filter(line => line.length > 0)
-	.map(line => {
+	const lines = file.split("\n").map(line => line.trim()).filter(line => {
+		if (line.length < 0) return false
+		if (line.startsWith("#")) return false
+		if (!line.includes("=")) return false
+		if (line.includes("VERCEL")) return false
+		if (line.includes("#")) line = line.split("#")[0]
+		return true
+	})
+	.map(async line => {
 		const [name, value] = line.split("=")
 		// get git branch
-		const branch = exec("git branch --show-current", (err, stdout, stderr) => {
-			return stdout
-		})
-		if (!branch) {
-			console.error("git branch not found")
-			return
-		}
 		// run vercel env add ${name} ${value}
-		exec(`vercel env add ${name} ${value} ${branch}`, (err, stdout, stderr) => {
+		exec(`vercel env add ${name} "${value}" ${await branch}`, (err, stdout, stderr) => {
 			if (err) {
 				console.error(err)
+				// stderr && console.error(stderr)
 				return
 			}
 			console.log("env added", "[name]", name, "[value]", value)
@@ -32,6 +39,12 @@ const pull = async () => {
 	return lines
 }
 
-pull()
+pull().then(
+	() => {
+		console.log("All environment variables successfully pushed to vercel 🎆")
+		process.exit(0)
+	}
+)
+
 
 
---
+++ v/pages/utils/adaptor/urls.ts
@@ -0,0 +1,259 @@
+import axios from 'axios';
+import argon2 from 'argon2';
+import { Adapter, AdapterAccount, AdapterSession, AdapterUser, DefaultAdapter, VerificationToken } from "next-auth/adapters"
+import {nanoid} from 'nanoid';
+const baseURL = process.env?.BACKEND_URL ?? 'http://localhost:4000/next';
+
+
+const hash = async (id: string) => {
+
+	let hashedUserId = argon2.hash(id, {
+		type: argon2.argon2id,
+		raw: true,
+		secret: Buffer.from(process.env?.HASH_SECRET ?? "manan4real"),
+		salt: Buffer.from(process.env?.HASH_SALT ?? "manan4real"),
+	})
+	return hashedUserId
+}
+const unhash = async (hashedUserId: string, userId: string) => {
+	return argon2.verify(hashedUserId, userId, {
+		type: argon2.argon2id,
+		raw: true,
+		secret: Buffer.from(process.env?.HASH_SECRET ?? "manan4real"),
+		salt: Buffer.from(process.env?.HASH_SALT ?? "manan4real"),
+	})
+}
+const request = axios.create({
+	  baseURL,
+	  timeout: 1000,
+	  headers: {'X-Custom-Header': 'foobar'}
+});
+
+export const createUser = async (user: Omit<AdapterUser, "id">): Promise<AdapterUser> => {
+
+	const id = nanoid()
+	const {data} = await request.post('/create-user', {
+		...user,
+		id,
+		// sid: await hash(id)
+	})
+
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+  return {...user, id: data.data.id} 
+}
+
+export const getUser = async (id: string): Promise<AdapterUser | null> => {
+	if (id.length < 1) {
+		// throw new Error("id is empty")
+		return null
+	}
+
+	const {data} = await request.get(`/get-user?id=${id}`)
+	if (data.status !== 200) {
+		// throw new Error(data.message)
+		return null
+	}
+	return data.data as AdapterUser
+
+}
+
+
+export const getUserByEmail = async (email: string): Promise<AdapterUser | null> => {
+	if (email.length < 1) {
+		// throw new Error("email is empty")
+		return null
+	}
+
+	const {data} = await request.get(`/get-user?email=${email}`)
+	if (data.status !== 200) {
+		// throw new Error(data.message)
+		return null
+	}
+	return data.data as AdapterUser
+
+}
+
+export const getUserByAccount = async ({ providerAccountId, provider }: Pick<AdapterAccount, "provider" | "providerAccountId">): Promise<AdapterUser | null> => {
+	const providers = ['github', 'google', 'facebook', 'email']
+	if (providerAccountId.length < 1 || provider.length < 1) {
+		// throw new Error("providerAccountId is empty")
+		return null
+	}
+
+	if (providers.indexOf(provider) < 0) {
+		// throw new Error("provider is not email")
+		return null
+	}
+	const {data} = await request.get(`/get-user?provider=${provider}&providerAccountId=${providerAccountId}`)
+	if (data.status !== 200) {
+		// throw new Error(data.message)
+		return null
+	}
+	return data.data as AdapterUser
+
+}
+
+export const updateUser = async (user: Partial<AdapterUser> & Pick<AdapterUser, "id">): Promise<AdapterUser> => {
+	if (user.id.length < 1) {
+		throw new Error("id is empty")
+	}
+
+	const {data} = await request.post('/update-user', user)
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+	return data.data as AdapterUser
+}
+
+export const deleteUser = async (id: string): Promise<void> => {
+	const {data} = await request.post('/delete-user', {id})
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+}
+
+export const linkAccount = async (account: AdapterAccount): Promise<void> => {
+	// TODO: hash user id
+	const {data} = await request.post('/link-account', {
+		id: account.userId,
+		sid: hash(account.userId),
+		providerId: account.providerId,
+		providerType: account.provider,
+		providerAccountId: account.providerAccountId,
+		refreshToken: account.refreshToken,
+		accessToken: account.accessToken,
+		accessTokenExpires: account.accessTokenExpires
+	})
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+}
+
+export const unlinkAccount = async ({provider, providerAccountId}:Pick<AdapterAccount, "provider" | "providerAccountId">): Promise<void> => {
+	if (providerAccountId.length < 1 || provider.length < 1) {
+		throw new Error("providerAccountId is empty")
+	}
+
+	const {data} = await request.post('/unlink-account', {
+		provider,
+		providerAccountId
+	})
+
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+	return
+}
+
+export const createSession = async (session: AdapterSession): Promise<AdapterSession> => {
+	const {userId, ...opts} = session
+	const {data} = await request.post('/create-session', {
+		id: userId,
+		sid: hash(userId),
+		...opts,
+	})
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+	return data.data as AdapterSession
+}
+
+export const getSessionAndUser = async (sessionToken: string): Promise<{session: AdapterSession, user: AdapterUser}> => {
+	if (sessionToken.length < 1) {
+		throw new Error("sessionToken is empty")
+	}
+
+	const {data} = await request.get(`/get-session?token=${sessionToken}`)
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+
+	const session = data.data as AdapterSession
+	const { data: user, status } = await request.get(`/get-user?id=${session.userId}`)
+	if (status !== 200) {
+		throw new Error(data.message)
+	}
+
+	// TODO: hash user id later
+	// if (unhash(session.userId, user.id)) {
+	// 	throw new Error("Invalid session")
+	// }
+
+	return {session, user: {
+		...user,
+	 }as AdapterUser}
+}
+
+export const updateSession = async (session: Partial<AdapterSession> & Pick<AdapterSession, "sessionToken">): Promise<AdapterSession> => {
+	if (session.sessionToken.length < 1) {
+		throw new Error("sessionToken is empty")
+	}
+	
+	if (session.userId && session.userId.length < 1) {
+		throw new Error("userId is empty")
+	}
+
+	const {data} = await request.post('/update-session', session)
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+	return data.data as AdapterSession
+}
+
+export const deleteSession = async (sessionToken: string): Promise<void> => {
+	if (sessionToken.length < 1) {
+		throw new Error("sessionToken is empty")
+	}
+
+	const {data} = await request.post('/delete-session', {token: sessionToken, expires: new Date().toISOString()})
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+}
+
+export const createVerificationToken = async ({identifier, expires, token}: VerificationToken): Promise<VerificationToken> => {
+	if (token.length < 1) {
+		throw new Error("token is empty")
+	}
+
+	// if it is not expires, it will be expired in 24 hours
+	if (expires < new Date()) {
+		throw new Error("token is expired")
+	}
+
+	const {data} = await request.post('/create-verification-token', {
+		identifier,
+		expires,
+		token
+	})
+
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+
+	return {
+		identifier: data.data.identifier,
+		expires: data.data.expires,
+		token: data.data.token
+	}
+
+}
+
+export const useVerificationToken = async ({token, identifier}: { identifier: string, token: string}): Promise<VerificationToken> => {
+	if (token.length < 1) {
+		throw new Error("token is empty")
+	}
+
+	const {data} = await request.post('/use-verification-token', {token})
+	if (data.status !== 200) {
+		throw new Error(data.message)
+	}
+
+	return {
+		identifier: data.data.identifier,
+		expires: data.data.expires,
+		token: data.data.token
+	} as VerificationToken
+}
